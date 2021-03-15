9b56cbe965c4c1e382261b18edf97584994462d9
Basudev Paudel <basudev.paudel@itonics.de>
2021-03-15 21:00:50 +0545

    Verification for all links

--- app/Http/Controllers/QRScanner.php
+++ app/Http/Controllers/QRScanner.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class QRScanner extends Controller {
+  public function __construct() {
+    $this->middleware(['auth','verified']);
+  }
     public function scanner() {
       return view('qrscanner');
     }
